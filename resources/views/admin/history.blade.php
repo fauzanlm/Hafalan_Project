51b46e503ee673a4ec6c3efca4c78502e08ce165
Muhammad Fauzan <fauzan140720004@gmail.com>
2021-06-28 08:59:00 +0700

    menambahkan file history

---
+++ resources/views/admin/history.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+@section('title', 'History - Admin')
+
+@section('container')
+    <!-- Begin Page Content -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="container-fluid">
+
+            </div>
+        </div>
+    </div>
+@endsection
